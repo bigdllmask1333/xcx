537e4bd120468ef3810559ab37a438e8274c6f13
cww0128 <1113249273@qq.com>
2019-11-14 23:37:14 +0800

    catch

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,8 +6,31 @@ class Index
     public function index()
     {
        //采集某页面所有的图片
-       $data = QueryList::get('http://www.chengxu.la/')->find('img')->attrs('src');
+       $data = QueryList::get('http://www.chengxu.la/category-31.html')->find('img')->attrs('src');
        //打印结果
        print_r($data->all());
+    }
+
+    public function getdata(){
+    	$url = 'http://www.chengxu.la/category-31.html';
+		// 定义采集规则
+		$rules = [
+		    // 采集文章标题
+		    'title' => ['.item a','title'],
+		    'href' => ['.item a','href'],
+		    'img1' => ['.item a img','src'],
+		    'img2' => ['.item a img','data-original'],
+		    'text' => ['.item a em','text'],
+		    'maintitle' => ['.item a span','text'],
+		    // // 采集文章作者
+		    // 'author' => ['#author_baidu>strong','text'],
+		    // // 采集文章内容
+		    // 'content' => ['.post_content','html']
+		];
+		$rt = QueryList::get($url)->rules($rules)->query()->getData();
+
+		echo "<pre>";
+		print_r($rt->all());
+		echo "</pre>";
     }
 }
