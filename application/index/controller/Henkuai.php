a59cec4acb1fa3a610f9a5908ab03ee5da88fbf8
cww0128 <1113249273@qq.com>
2019-11-18 23:36:30 +0800

    henkuai

--- application/index/controller/Henkuai.php
+++ application/index/controller/Henkuai.php
@@ -15,17 +15,52 @@ class Henkuai {
 		return 'just test';
 	}
 
+	/*TODO  循环遍历出所有数据*/
+
+
+
+
+	/*通过分类获取列表*/
+	//http://daohang.henkuai.com/program/1  直接拿数据
+	public function getlistbytype(){
+		$url = 'http://daohang.henkuai.com/program/1';
+		$ql = QueryList::get($url);
+
+
+
+
+
+		$rt = $ql->rules($rules)->query()->getData();
+		echo "<pre>";
+		print_r($rt->all());
+		echo "</pre>";
+	}
+
+	// http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&page=2&flag=1&sort=new&apptype=1
+	// http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index&id=1
+
+
+
 	//社交 http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index&id=1
 	//资讯 http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index&id=3
 	//全部 http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index
 	// http://xcx3.com/index.php/index/henkuai/getall
-	public function getall() {
+	// 这是通过接口获取列表
+	public function getlist() {
+
+		/*倒序循环10页*/
 
 		$url = 'http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index&id=1';
+		// $url = 'http://daohang.henkuai.com/ajaxHandle.php?handle=getlist&from=index&id=1';
 
 		$ql = QueryList::get($url);
 
 		$cdata = json_decode($this->juhecurl($url), true);
+
+		if(!$cdata['html']){
+			echo '暂无数据'
+			return ;
+		}
 		$ql = QueryList::html($cdata['html']);
 		$images = $ql->find('a div img')->attrs('src')->all();
 
@@ -78,5 +113,36 @@ class Henkuai {
 		$httpInfo = array_merge($httpInfo, curl_getinfo($ch));
 		curl_close($ch);
 		return $response;
+	}
+
+	/*获取详情*/
+	// http://xcx.com/index.php/index/henkuai/getDetail
+	public function getDetail(){
+		$url = 'http://daohang.henkuai.com/detail/8125.html';
+		$ql = QueryList::get($url);
+
+		$title = $ql->find('.main .main-cont .main-cont-left .clearfix h1')->text();
+		$tags = $ql->find('.main .main-cont .main-cont-left .clearfix a')->attrs('href')->all();
+		$qrcode = $ql->find('.main .main-cont .ewmcode img')->src;
+		$images = $ql->find('.main .main-cont .match-list img')->attrs('src')->all();
+		$remark = $ql->find('.main>div:eq(2) .screenshot')->text();
+		$typesurl = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->href;
+		$typesname = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->text();
+
+		echo "<pre>";
+		print_r($title);
+		echo "<hr>";
+		print_r($tags);
+		echo "<hr>";
+		print_r($qrcode);
+		echo "<hr>";
+		print_r($images);
+		echo "<hr>";
+		print_r($remark);
+		echo "<hr>";
+		print_r($typesurl);
+		echo "<hr>";
+		print_r($typesname);
+		echo "</pre>";
 	}
 }
