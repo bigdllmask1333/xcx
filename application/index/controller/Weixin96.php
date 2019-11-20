8cffcac0fac8f6ebb1ab391f3568c9dc46845ec8
cww0128 <1113249273@qq.com>
2019-11-20 13:47:45 +0800

    96weixin detail end

--- application/index/controller/Weixin96.php
+++ application/index/controller/Weixin96.php
@@ -46,73 +46,33 @@ class Weixin96 {
 		echo "</pre>";
 	}
 
-	/**
-	 * 请求接口返回内容
-	 * @param  string $url [请求的URL地址]
-	 * @param  string $params [请求的参数]
-	 * @param  int $ipost [是否采用POST形式]
-	 * @return  string
-	 */
-	public function juhecurl($url, $params = false, $ispost = 0) {
-		$httpInfo = array();
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'JuheData');
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		if ($ispost) {
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-			curl_setopt($ch, CURLOPT_URL, $url);
-		} else {
-			if ($params) {
-				curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
-			} else {
-				curl_setopt($ch, CURLOPT_URL, $url);
-			}
-		}
-		$response = curl_exec($ch);
-		if ($response === FALSE) {
-			//echo "cURL Error: " . curl_error($ch);
-			return false;
-		}
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$httpInfo = array_merge($httpInfo, curl_getinfo($ch));
-		curl_close($ch);
-		return $response;
-	}
-
 	/*获取详情*/
 	// http://xcx.com/index.php/index/henkuai/getDetail
 	public function getDetail() {
-		$url = 'http://daohang.henkuai.com/detail/8125.html';
+		$url = 'http://xcx.96weixin.com/gouwu/4050.html';
 		$ql = QueryList::get($url);
 
-		$title = $ql->find('.main .main-cont .main-cont-left .clearfix h1')->text();
-		$tags = $ql->find('.main .main-cont .main-cont-left .clearfix a')->attrs('href')->all();
-		$qrcode = $ql->find('.main .main-cont .ewmcode img')->src;
-		$images = $ql->find('.main .main-cont .match-list img')->attrs('src')->all();
-		$remark = $ql->find('.main>div:eq(2) .screenshot')->text();
-		$typesurl = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->href;
-		$typesname = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->text();
+		$title = $ql->find('.software_detail_title .software_detail_desc h1')->text();
+		$qrcode = $ql->find('.software_detail_title .software_detail-code img')->src;
+		$icon = $ql->find('.software_detail_title .software_detail_icon img')->src;
+		$images = $ql->find('.soft_detail ul li img')->attrs('src')->all();
+		$remark = $ql->find('.minapp_function div')->text();
+		$tags = $ql->find('.software_detail_title .software_detail-desc .long-p:contains("搜索词："))')->text();
+		// $typesurl = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->href;
+		// $typesname = $ql->find('.main>div:eq(0) .main-cont-left>div:eq(1) ul>li:eq(0) a')->text();
 
 		echo "<pre>";
 		print_r($title);
 		echo "<hr>";
-		print_r($tags);
-		echo "<hr>";
 		print_r($qrcode);
+		echo "<hr>";
+		print_r($icon);
 		echo "<hr>";
 		print_r($images);
 		echo "<hr>";
 		print_r($remark);
 		echo "<hr>";
-		print_r($typesurl);
-		echo "<hr>";
-		print_r($typesname);
+		print_r($tags);
 		echo "</pre>";
 	}
 }
